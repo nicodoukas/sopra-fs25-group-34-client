de27e448dcb7af8a38fad5ff38727417e2b9f2d3
monolino <jul.wuersch@gmail.com>
2025-02-28 10:11:29 +0100

    added birthday and creationdate (as string)

--- app/types/user.ts
+++ app/types/user.ts
@@ -2,6 +2,8 @@ export interface User {
   id: string | null;
   name: string | null;
   username: string | null;
+  birthday: string | null;
+  creationdate: string | null;
   token: string | null;
   status: string | null;
 }
