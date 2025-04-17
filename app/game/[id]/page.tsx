512446b99bc3915047044df31d108b72e7f37af2
monolino <jul.wuersch@gmail.com>
2025-04-17 17:49:27 +0200

    Added addButton to gamePage and SongCard. #20 #19

--- app/game/[id]/page.tsx
+++ app/game/[id]/page.tsx
@@ -3,7 +3,7 @@
 import React, {useEffect, useState} from "react";
 import { useRouter, useParams } from "next/navigation";
 import {useApi} from "@/hooks/useApi";
-import { Button, Typography, Card, List } from "antd";
+import { Button, Typography, Card } from "antd";
 import "@ant-design/v5-patch-for-react-19";
 import {Game} from "@/types/game";
 import {Player} from "@/types/player";
@@ -20,8 +20,11 @@ const GamePage = () => {
   const gameId = params.id;
   const [game, setGame] = useState<Game>({} as Game);
   const [player, setPlayer] = useState<Player>({} as Player);
-  const [audioState, setAudioState] = useState<Boolean>(true);
-  const [isPlaying, setIsPlaying] = useState<Boolean>(false);
+  const [audioState, setAudioState] = useState<Boolean>(true); //True if song not yet played, false otherwise
+  const [isPlaying, setIsPlaying] = useState<Boolean>(false); //True if song is currently playing
+  const [isFlipped, setIsFlipped] = useState<number | null>(null); //index of flipped SongCard
+  const [placement, setPlacement] = useState<number | null>(null); //position of placement of SongCard
+  const [songCard, setSongCard] = useState<SongCard | null>({} as SongCard); // SongCard of currentRound
 
 
   const playAudio = async (): Promise<void> => {
@@ -32,12 +35,31 @@ const GamePage = () => {
     audio.onended = () => {setAudioState(false); setIsPlaying(false)}
   }
 
+  const flipCard = function (index: number) {
+    if (isFlipped === index) {setIsFlipped(null); return;} // if card already flipped
+    setIsFlipped(index)
+  }
+
+  const addCard = function (index: number) {
+    setPlacement(index);
+
+    console.log("CardPlacement: " + placement);
+  }
+
+  const confirmPlacement = async (): Promise<void> => {
+    const body = {
+      "songCard": songCard,
+      "position": placement,
+    }
+  }
+
   useEffect(() => {
     const fetchData = async () => {
       try {
         const gameData = await apiService.get<Game>(`/games/${gameId}`);
         console.log("This is the game: ", game);
         setGame(gameData);
+        setSongCard(gameData.currentRound?.songCard);
         const userId = localStorage.getItem("id");
         const playerData = await apiService.get<Player>(`/games/${gameId}/${userId}`);
         setPlayer(playerData);
@@ -75,7 +97,7 @@ const GamePage = () => {
           flexDirection: "column",
           justifyContent: "space-between",
           alignItems: "center",
-          padding: "40px",
+          paddingTop: "20px",
           width: "700px",
           textAlign: "center",
           borderRadius: "16px",
@@ -85,8 +107,8 @@ const GamePage = () => {
         <Title level={2} style={{ color: "#1890ff" }}>
           {game?.gameName || "{gameName}"}
         </Title>
-        <div className="playButtoncontainer">
-        {game?.currentRound?.songCard?.songURL && (
+        <div className="playButtonContainer">
+        {songCard?.songURL && (
           <div className={`playButton ${isPlaying ? "playing" : ""}`}
                onClick={audioState && !isPlaying ? playAudio : undefined}
                style={{pointerEvents: audioState ? "auto" : "none"}}>
@@ -94,23 +116,65 @@ const GamePage = () => {
           </div>
         )}
         </div>
-        <div className="timeline">
+      <div className="songCardContainer">
+        {placement == null && player?.id == game?.currentRound?.activePlayer?.id && (
+          <div className="songCard">
+          <Text strong>?</Text>
+          </div>
+        )}
+
+      </div>
+
+        <div>
           <Title level={4} style={{textAlign: "center"}}>Your Timeline</Title>
-          {player?.timeline && player.timeline.length > 0 ? (
-            <List
-              bordered
-              dataSource={player.timeline}
-              renderItem={(card: SongCard, index: number) => (
-                <List.Item key={index}>
-                  <Text strong>{card.year}</Text>
-                  <Text type="secondary">{card.title}</Text>
-                </List.Item>
-              )}
-            />
+          {player?.timeline && player?.timeline?.length > 0 ? (
+            <div className="timeline">
+              {player.timeline.map((card: SongCard, index:number) => (
+                <React.Fragment key={index}>
+                  {player?.id == game?.currentRound?.activePlayer?.id && (
+                    placement == index ? (
+                      <div className="songCard">
+                        <Text strong>?</Text>
+                      </div>
+                    ) : (
+                      <div className="addButton" onClick={() => addCard(index)}>
+                        <img src="/img/plus.png" alt="add" className="plusIcon"/>
+                      </div>
+                    )
+                  )}
+
+                  <div key={index} onClick={() => flipCard(index)} className="songCard">
+                    {isFlipped === index ? (
+                      <>
+                        <Text strong style={{fontSize: "14px"}}>{card.title}</Text>
+                        <Text type="secondary" style={{fontSize: "14px"}}>{card.artist}</Text>
+                      </>
+                    ) : (
+                      <Text strong>{card.year}</Text>
+                    )}
+                  </div>
+                </React.Fragment>
+              ))}
+                <div>
+                  {player?.id == game?.currentRound?.activePlayer?.id && (
+                    placement == player?.timeline?.length ? (
+                      <div className="songCard">
+                        <Text strong>?</Text>
+                      </div>
+                    ) : (
+                      <div className="addButton" onClick={() => addCard(player.timeline.length)}>
+                        <img src="/img/plus.png" alt="add" className="plusIcon"/>
+                      </div>
+                    )
+                  )}
+
+                </div>
+            </div>
           ) : (
             <Text type="secondary">No songcards in timeline.</Text>
           )}
         </div>
+        <Button onClick={() => confirmPlacement}>Confirm</Button>
         <Button style={{ marginTop: "30px" }} onClick={() => router.back()}>
           Back to Lobby-Screen
         </Button>
