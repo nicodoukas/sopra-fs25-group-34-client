dbb0a11c980692a23e891bd42e960aa898aa6f8c
nicodoukas <nico.doukas@uzh.ch>
2025-04-23 17:02:42 +0200

    change deployment url for websocketservice

--- app/websocket/websocketService.tsx
+++ app/websocket/websocketService.tsx
@@ -5,9 +5,8 @@ import SockJS from "sockjs-client";
 type OnMessageCallback = (message: string) => void;
 
 export const connectWebSocket = (onMessageCallback: OnMessageCallback, gameId: string | string[] | undefined) => {
-  const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
   const client = new Client({
-    webSocketFactory: () => new SockJS(`${protocol}://sopra-fs25-group-34-server.oa.r.appspot.com/ws`),
+    webSocketFactory: () => new SockJS(`https://sopra-fs25-group-34-server.oa.r.appspot.com/ws`),
     connectHeaders: {
     },
     onConnect: () => {
