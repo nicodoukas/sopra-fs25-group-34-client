68e633f0b91bc6de59004354d4e40e6dbdb0aad1
nicodoukas <nico.doukas@uzh.ch>
2025-04-23 16:43:38 +0200

    websocketService update production URL

--- app/websocket/websocketService.tsx
+++ app/websocket/websocketService.tsx
@@ -5,8 +5,9 @@ import SockJS from "sockjs-client";
 type OnMessageCallback = (message: string) => void;
 
 export const connectWebSocket = (onMessageCallback: OnMessageCallback, gameId: string | string[] | undefined) => {
+  const protocol = window.location.protocol === 'https:' ? 'wss' : 'ws';
   const client = new Client({
-    webSocketFactory: () => new SockJS("http://localhost:8080/ws"),
+    webSocketFactory: () => new SockJS("`${protocol}://sopra-fs25-group-34-server.oa.r.appspot.com/ws`"),
     connectHeaders: {
     },
     onConnect: () => {
